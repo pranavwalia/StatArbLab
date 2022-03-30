0390cead065eac5057a52917c291c19b9abfd77c
Pranav Ahluwalia <ahluwalia.pr@husky.neu.edu>
2022-03-30 16:07:10 -0400

    testSetPairsData

--- PairsTrading/TestDistanceApproach.py
+++ PairsTrading/TestDistanceApproach.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from re import I
+from turtle import distance
 from DistanceApproach import DistanceApproach
 import unittest
 import pandas as pd
@@ -7,17 +9,21 @@ class TestDistanceApproach(unittest.TestCase):
 
     def testSetPairsData(self):
         distanceClass = DistanceApproach()
-        pass
-        #Not enough columns
-        
+        #Not Enough Columns in DF
+        dfNotEnoughColumns = pd.DataFrame({'Dates' : ['date']})
+        self.assertRaises(Exception,distanceClass.setPairsData,dfNotEnoughColumns) 
         #First column not datetime
-
+        dfFirstNotDateTime = pd.DataFrame({'Price' : [1], 'Date' : [datetime(2020,4,5)]})
+        self.assertRaises(Exception, distanceClass.setPairsData, dfFirstNotDateTime)
         #Non-numerical column
+        dfNoneNumericalColumn = pd.DataFrame({'Date' : [datetime(2020,1,2)], 'Price1' : [1], 'Price2' : ['dog']})
+        self.assertRaises(Exception, distanceClass.setPairsData, dfNoneNumericalColumn)
+        print('SetPairsData Test Passed')
+
 
     def testSetTradeData(self):
-        distanceClass = DistanceApproach()
-        self.assertEqual(1,1)
-         
+        pass
+        
          
 
 if __name__ == '__main__':
